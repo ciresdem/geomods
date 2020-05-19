cb5bc2b015c7e9a2fbe9a13ec7ecd7e723c989e2
user <matthew.love@colorado.edu>
2020-05-19 11:54:21 -0700

    x360.py update to waffles

--- scripts/x360.py
+++ scripts/x360.py
@@ -8,18 +8,16 @@ if __name__ == '__main__':
     which_way = None
     try:
         infile = sys.argv[1]
-        outfile = sys.argv[2]
     except:
-        print "you must enter an in and outfile; x360.py input output"
+        print "you must enter an infile; x360.py input"
         sys.exit(0)
         
-    if len(sys.argv) > 3: which_way = sys.argv[3]
+    if len(sys.argv) > 2: which_way = sys.argv[2]
     with open(infile, 'r') as iob:
-        with open(outfile, 'w') as oob:
-            for xyz in waffles.xyz_parse(iob):
-                if which_way is None: x = xyz[0] -= 360
-                else: x = xyz[0] += 360
-                waffles.xyz_line([x, xyz[1], xyz[2]], oob)
+        for xyz in waffles.xyz_parse(iob):
+            if which_way is None: x = xyz[0] - 360
+            else: x = xyz[0] + 360
+            waffles.xyz_line([x] + xyz[1:])
 
 ### End
 
