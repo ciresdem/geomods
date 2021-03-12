4b1a4ffb28ef174d94152108c47b893cfad5ab6f
matthew love <matthew.love@colorado.edu>
2021-03-12 13:58:58 -0800

    add colortable to setup.py

--- setup.py
+++ setup.py
@@ -64,6 +64,7 @@ setuptools.setup(
         'scripts/gdal_query.py',
         'scripts/gdal_gquery.py',
         'scripts/gdal_perspective.py',
+        'scripts/colortable.py',
         'scripts/vdatum_cmd.py',
         'scripts/smooth_dem_bathy.py',
         'scripts/xyz_clip.py',
